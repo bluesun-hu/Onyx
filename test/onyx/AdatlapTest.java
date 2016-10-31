3d5a8709443459fd09411808e8d6ed0a9e52e91b
bluesun <piri70@gmail.com>
2016-10-31 23:22:23 +0100

    Test package added (Adatlap)
    Unit test for Adatlap.java

---
+++ test/onyx/AdatlapTest.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onyx;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.ItemEvent;
+import javax.swing.event.DocumentEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author PIRI
+ */
+public class AdatlapTest {
+    
+    public AdatlapTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of insertUpdate method, of class Adatlap.
+     */
+    @Test
+    public void testInsertUpdate() {
+        System.out.println("insertUpdate");
+        DocumentEvent e = null;
+        Adatlap instance = null;
+        instance.insertUpdate(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removeUpdate method, of class Adatlap.
+     */
+    @Test
+    public void testRemoveUpdate() {
+        System.out.println("removeUpdate");
+        DocumentEvent e = null;
+        Adatlap instance = null;
+        instance.removeUpdate(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of changedUpdate method, of class Adatlap.
+     */
+    @Test
+    public void testChangedUpdate() {
+        System.out.println("changedUpdate");
+        DocumentEvent e = null;
+        Adatlap instance = null;
+        instance.changedUpdate(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of itemStateChanged method, of class Adatlap.
+     */
+    @Test
+    public void testItemStateChanged() {
+        System.out.println("itemStateChanged");
+        ItemEvent e = null;
+        Adatlap instance = null;
+        instance.itemStateChanged(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of actionPerformed method, of class Adatlap.
+     */
+    @Test
+    public void testActionPerformed() {
+        System.out.println("actionPerformed");
+        ActionEvent e = null;
+        Adatlap instance = null;
+        instance.actionPerformed(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of focusGained method, of class Adatlap.
+     */
+    @Test
+    public void testFocusGained() {
+        System.out.println("focusGained");
+        FocusEvent e = null;
+        Adatlap instance = null;
+        instance.focusGained(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of focusLost method, of class Adatlap.
+     */
+    @Test
+    public void testFocusLost() {
+        System.out.println("focusLost");
+        FocusEvent e = null;
+        Adatlap instance = null;
+        instance.focusLost(e);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
